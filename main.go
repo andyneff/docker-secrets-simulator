38118dc69b3058ce52993276efbbced65ccb4184
Andy Neff <andyneff@users.noreply.github.com>
2017-09-28 17:54:18 -0400

    Add ability to specify mode

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "io/ioutil"
     "fmt"
     "sort"
+    "strconv"
 )
 
 func main() {
@@ -13,12 +14,19 @@ func main() {
   ignores := []string{ "GOLANG_VERSION", "GOPATH", "HOME", "HOSTNAME", "PATH",
                        "TERM", "no_proxy" }
 
+  mode := os.FileMode(0644)
+
+  if len(os.Args) > 1 {
+    mode_int, _ := strconv.ParseInt(os.Args[1], 8, 64)
+    mode = os.FileMode(mode_int)
+  }
+
   for _, e := range os.Environ() {
     pair := strings.Split(e, "=")
     if !contains(ignores, pair[0]) {
       contents := []byte(pair[1]);
-      err := ioutil.WriteFile("/run/secrets/" + pair[0], contents, 0644)
-      check(err) 
+      err := ioutil.WriteFile("/run/secrets/" + pair[0], contents, mode)
+      check(err)
     }
   }
 }
